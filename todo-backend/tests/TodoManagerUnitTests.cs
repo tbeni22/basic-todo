dd8a4d72994dd135e79a8b8ab339de85ae997fa3
tbeni22 <47601385+tbeni22@users.noreply.github.com>
2022-06-13 14:09:31 +0200

    Unit tests supplemented

--- todo-backend/tests/TodoManagerUnitTests.cs
+++ todo-backend/tests/TodoManagerUnitTests.cs
@@ -21,9 +21,6 @@ namespace tests
         {
             repo = new Mock<ITodoItemRepository>();
             manager = new TodoItemManager(repo.Object);
-
-            repo.Setup(r => r.GetItemById(It.IsAny<int>()))
-                .Returns((TodoItem)null);
         }
 
         [TestMethod]
@@ -36,7 +33,7 @@ namespace tests
 
             bool result = manager.MoveItem(item);
 
-            Assert.IsFalse(result, "Move should fail becouse of the non-existent item.");
+            Assert.IsFalse(result, "Move should fail because of the non-existent item.");
             repo.Verify(r => r.GetItemById(It.IsAny<int>()), Times.Once);
         }
 
@@ -45,8 +42,27 @@ namespace tests
         {
             bool result = manager.MoveItem(new TodoItem());
 
-            Assert.IsFalse(result, "Move should fail becouse of the null value of OrderNumber.");
+            Assert.IsFalse(result, "Move should fail because of the null value of OrderNumber.");
             repo.Verify(r => r.GetItemById(It.IsAny<int>()), Times.Never);
+        }
+
+        [TestMethod]
+        public void Test_SuccessfulMove()
+        {
+            List<TodoItem> items = new List<TodoItem>();
+            items.Add(new TodoItem(1, "a", ".", null, 1, "none"));
+            items.Add(new TodoItem(2, "b", "", null, 2, "none"));
+            repo.Setup(r => r.GetItemById(1)).Returns(items[0]);
+            repo.Setup(r => r.GetItemByOrderNumber(2)).Returns(items[1]);
+            var itemToMove = new TodoItem { ID = 1, OrderNumber = 2 };
+
+            bool result = manager.MoveItem(itemToMove);
+
+            Assert.IsTrue(result, "Move should execute successfully.");
+            repo.Verify(r => r.UpdateItem(It.Is<TodoItem>(ti => ti.ID == 1 && ti.OrderNumber == 2)), 
+                Times.Once, "Item should be moved in database.");
+            repo.Verify(r => r.UpdateItem(It.Is<TodoItem>(ti => ti.ID == 2 && ti.OrderNumber == 1)), 
+                Times.Once, "Colliding item should also be moved.");
         }
     }
 }
